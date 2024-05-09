b4c2539e557a6304d2888ac7a4a58ef27bd419f4
karanx64 <karansinghx64@gmail.com>
2024-05-09 12:16:38 +0530

    v

--- f1.py
+++ f1.py
@@ -1,8 +1,38 @@
-i = 2
-while (i<100):
-    j=2
-    while(j<=(i/j)):
-        if not (i%j) : break
-        j=j+1
-    if(j> (i/j)): print(i, end=" ")
-    i=i+1
+#variables declaration
+age = 12
+name = 'karan'
+gpa = 1.2
+student = True
+
+# type() function
+
+print("age is",type(age))
+print("name is",type(name))
+print("gpa is",type(gpa))
+print("student is",type(student))
+
+print("")
+
+# explicit conversion
+
+age = float(age)
+name = bool(name)
+gpa = int(gpa)
+student = str(student)
+
+print("age is",type(age))
+print("name is",type(name))
+print("gpa is",type(gpa))
+print("student is",type(student))
+
+print("")
+# implicit conversion
+
+num1 = 5
+num2 = 1.1
+num3 = num1/num2
+print(num3)
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
